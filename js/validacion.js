375f2d3691ddc9d82c428152e0b921bca6467953
ma-emilia <mariaemilialeguizamon@gmail.com>
2022-07-28 21:24:51 -0300

    Form al email-1

--- js/validacion.js
+++ js/validacion.js
@@ -1,18 +1,39 @@
 function validacion(){
     var nombre = document.getElementById("nombre").value;
-    if(nombre == ""){
-        alert("Por favor, completar el Nombre.");
-        return false;
-    }
     var email = document.getElementById("email").value;
-    if(email == ""){
-        alert("Por favor, completar el E-mail.");
-        return false;
-    }
     var asunto = document.getElementById("asunto").value;
-    if(asunto == ""){
-        alert("Por favor, completar el asunto.");
+    var benvio = document.getElementById("boton1");
+    var bconfirmar = document.getElementById("boton2");
+    if(nombre == "" || email == "" || asunto == ""){
+        alert("Por favor, completa todos los campos señalados con asterisco");
         return false;
+    } 
+    else{
+        benvio.style.cssText = 'display: none';
+        bconfirmar.style.cssText= 'display: block';
     }
-  
+    
+}
+
+const $form = document.querySelector('#form')
+$form.addEventListener ('submit', handleSubmit)
+var benvio = document.getElementById("boton1");
+var bconfirmar = document.getElementById("boton2");
+
+async function handleSubmit(event){
+    event.preventDefault()
+    const form = new FormData(this)
+    const response = await fetch(this.action,{
+        method: this.method,
+        body: form,
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    if (response.ok){
+        this.reset()
+        alert('¡GRACIAS POR CONTACTARNOS! Te contestaremos pronto :) MyO')
+        benvio.style.cssText = 'display: block';
+        bconfirmar.style.cssText= 'display: none';
+    }
 }
